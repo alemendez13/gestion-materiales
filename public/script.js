971065e2448917485cb712fe885af2f9ccace45d
alemendez13 <130239389+alemendez13@users.noreply.github.com>
2025-10-21 09:36:04 -0600

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -201,7 +201,7 @@ const generatePDF = async (payload, catalogItem) => {
     const logoImageBytes = await fetch(logoUrl).then(res => res.arrayBuffer());
     const logoImage = await pdfDoc.embedPng(logoImageBytes);
 // SE REDUCE EL VALOR DE 0.25 A 0.15 PARA HACER EL LOGO MÁS PEQUEÑO
-    const logoDims = logoImage.scale(0.5);
+    const logoDims = logoImage.scale(0.15);
     page.drawImage(logoImage, {
         x: 50,
         y: height - logoDims.height - 50,
@@ -218,14 +218,14 @@ const generatePDF = async (payload, catalogItem) => {
 // SE CALCULA LA POSICIÓN X PARA CENTRAR EL TÍTULO
         x: (width - titleWidth) / 2, 
         // SE AJUSTA LA POSICIÓN Y PARA COLOCARLO DEBAJO DEL LOGO (APROX. 4 LÍNEAS = 80 PUNTOS)
-        y: height - logoDims.height - 50 - 200, 
+        y: height - logoDims.height - 50 - 100, 
         font,
         size: titleSize,
         color: rgb(0, 0, 0),
     });
 
     // Add Asset Details
-    const textY = height - 500;
+    const textY = height - 250;
     const details = [
         `Fecha: ${new Date().toLocaleDateString()}`,
         `ID del Activo: ${payload.assetId}`,
